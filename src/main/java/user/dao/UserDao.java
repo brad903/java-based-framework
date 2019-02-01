0ef9e7de69615900caf53a0fbe2e9c40d855846a
Brad Lee <brad903@naver.com>
2019-02-01 17:30:08 +0900

    UserDao 테스트 통과하도록 구현

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -1,6 +1,8 @@
 package user.dao;
 
 import core.jdbc.ConnectionManager;
+import core.jdbc.KeyHolder;
+import core.jdbc.PreparedStatementCreator;
 import org.slf4j.Logger;
 import user.domain.User;
 
@@ -10,8 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -20,6 +20,7 @@ public class UserDao {
 
     public void insert(User user) throws SQLException {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+
         try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
@@ -30,12 +31,29 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+            pstmt.executeUpdate();
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM USERS";
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            try(ResultSet rs = pstmt.executeQuery()) {
+                if(rs.next()) {
+                    User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email"));
+                    users.add(user);
+                    log.debug("user : {}", user);
+                }
+            }
+        }
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
